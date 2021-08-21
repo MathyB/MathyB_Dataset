ed6cac133095ddee5c259fbd4b4ca3c5932803ce
MathyB <89199328+MathyB@users.noreply.github.com>
2021-08-22 00:12:12 +0530

    Update MedSea.py

--- website/MedSea.py
+++ website/MedSea.py
@@ -225,7 +225,7 @@ def main():
 	c1,c2=st.columns(2)
 	
 	with c1:
-		data = st.text_area("", "scenario 1: min: x + y x, y >= 0 9000x + y >= 0 10000x + y >= 2000 x >= 2 scenario 2: min: x + y x, y >= 0 9.0E15x + 1.0E12y >= 0 1.1e16x + 1.0e12y >= 2.0e15 1.0e12x >= 2e12 for scenario 1, it works out. but for scenario 2 whose constrains are just scaled up by 1e12, the solver reports unbounded solution.") 
+		data = st.text_area("", "scenario 1: min: x + y x, y >= 0 9000x + y >= 0 10000x + y >= 2000 x >= 2 scenario 2: min: x + y x, y >= 0 9.0E15x + 1.0E12y >= 0 1.1e16x + 1.0e12y >= 2.0e15 1.0e12x >= 2e12 for scenario 1, it works out. but for scenario 2 whose constrains are just scaled up by 1e12, the solver reports unbounded solution.",height=500) 
 	
 		
 		if st.button("Run",key="1"):
