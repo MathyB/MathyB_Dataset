bf36ce7701423afdb472409e09b0e257f55ead4a
MathyB <89199328+MathyB@users.noreply.github.com>
2021-08-22 16:22:58 +0530

    Update MedSea.py

--- website/MedSea.py
+++ website/MedSea.py
@@ -216,7 +216,7 @@ def main():
 	
 	
 	#st.markdown( """<a style='display: block; text-align: center; font-family:times new roman; font-size:20px;' href="https://github.com/MathyB/MathyB_Dataset">Github Repository</a> """, unsafe_alhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQffhsjSSxdFA2TX4iQmSaw1eHkv1Zr1ZAPEQ&usqp=CAUlow_html=True, )
-	col1, col2 = st.columns(2)
+	col1, col2,col3,col4,col5 = st.columns(5)
 	with col1:
 		st.markdown("[![Github](https://bsmedia.business-standard.com/_media/bs/img/article/2020-02/13/full/1581607900-9541.jpg)](https://github.com/MathyB/MathyB_Dataset)")
 		
